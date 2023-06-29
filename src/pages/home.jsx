503df97a73eaaefba194bd23ca43aa583600b323
KSJ <kuldeepjhala03@gmail.com>
2023-06-29 19:54:17 +0530

    Navbar Responsiveness

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -4,11 +4,13 @@ import Home2 from "../components/home/home-2";
 import Home3 from "../components/home/home-3";
 import HomeServices from "../components/home/home-services";
 import Footer from "../components/footer";
+import Navbar from "../components/navbar";
 
 const Home = () => {
   return (
     <>
-      <NavbarPages title="Home" />
+      {/* <NavbarPages title="Home" /> */}
+      <Navbar title="Home"/>
       <Home1/>
       <Home2/>
       <Home3/>
